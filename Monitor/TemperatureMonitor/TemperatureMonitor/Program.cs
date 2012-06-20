fe4d2a9a96a6f36f068e31849694484a08afe7ca
Matt Barton <matt@mattbarton.org>
2012-06-20 14:56:31 +0100

    Add layout of Monitor project

---
+++ Monitor/TemperatureMonitor/TemperatureMonitor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using MattBarton.NETMF.Utilities;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.NetduinoPlus;
+
+namespace TemperatureMonitor
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			// write your code here
+			var test =  new HttpClient();
+
+		}
+
+	}
+}
